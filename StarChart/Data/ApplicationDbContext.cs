267ef7e0a901340673bf4d604b82b858f9cabf83
Matias Chaied <matias.chaied@southworks.com>
2021-04-12 11:01:47 -0300

    Create CelestialObject Model

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
