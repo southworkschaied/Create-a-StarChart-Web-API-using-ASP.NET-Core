a3f26191d0bee93dbc7343f9e3f8384bdb1decf7
Matias Chaied <matias.chaied@southworks.com>
2021-04-12 10:38:10 -0300

    Initialization

---
+++ StarChart/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StarChart.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
